d6730bd3ce5cd3b77768a36836830aa18141cea6
Philipp Dikmann <philipp.dikmann@ixds.com>
2016-10-22 11:23:02 +0200

    clean!

--- test/source.test.js
+++ test/source.test.js
@@ -3,12 +3,7 @@ var source = require( '../source.js' );
 
 describe( 'A failing test', function(){
   it( 'should fail', function(){
-    var to, r;
     test
-      .given( to = new source.Foo( "hey", 123 ))
-      .object( to )
-      .when( r = to.qux() )
-      .string( r )
-      .is( "ho456" )
+      .object( [] )
   })
 })
